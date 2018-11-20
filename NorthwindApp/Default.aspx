6646d236d898ead2251354e148d2e69c51216ed0
dcannon019688 <dcannon019688@DESKTOP-GUPHSG0>
2018-11-19 18:13:26 -0600

    Add project files.

---
+++ NorthwindApp/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NorthwindApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h1> EDIT SHIPPERS</h1>
+    <asp:GridView ID="gvShipper" AutoGenerateEditButton="true" AutoGenerateColumns="false"  runat="server">
+        <Columns>
+
+            <asp:TemplateField HeaderText="Id" ControlStyle-Width="10px">
+               <ItemTemplate>
+                   <asp:Label ID="lblId" runat="server" Text ='<%# Eval ("Id") %>'>  </asp:Label>
+               </ItemTemplate>
+            
+           </asp:TemplateField>
+
+           <asp:TemplateField HeaderText="Company Name" ControlStyle-Width="100px">
+               <ItemTemplate>
+                   <asp:Label ID="lblCompanyName" runat="server" Text ='<%# Eval ("CompanyName") %>'>  </asp:Label>
+               </ItemTemplate>
+               <EditItemTemplate>
+                   <asp:Textbox ID="txtCompanyName" runat="server" Text ='<%# Eval ("CompanyName") %>'>  </asp:Textbox>
+               </EditItemTemplate>
+           </asp:TemplateField>
+
+            <asp:TemplateField HeaderText="Phone" ControlStyle-Width="100px">
+               <ItemTemplate>
+                   <asp:Label ID="lblPhone" runat="server" Text ='<%# Eval ("Phone") %>'>  </asp:Label>
+               </ItemTemplate>
+               <EditItemTemplate>
+                   <asp:Textbox ID="txtPhone" runat="server" Text ='<%# Eval ("Phone") %>'>  </asp:Textbox>
+               </EditItemTemplate>
+                <FooterStyle HorizontalAlign="Right" />
+            <FooterTemplate>
+             <asp:Button ID="btnAddRow" runat="server" Text="Add New Row" />
+            </FooterTemplate>
+           </asp:TemplateField>
+            
+        </Columns>
+    </asp:GridView>
+
+
+</asp:Content>
---
+++ NorthwindApp/Default.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace NorthwindApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            gvShipper.RowEditing += GvShipper_RowEditing;
+            gvShipper.RowCancelingEdit += GvShipper_RowCancelingEdit;
+            gvShipper.RowUpdating += GvShipper_RowUpdating;
+            if (!IsPostBack)
+            {
+                GetAll();
+            }
+            
+
+        }
+
+        private void GvShipper_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = gvShipper.Rows[e.RowIndex];
+            TextBox txtCompanyName = (TextBox)row.FindControl("txtCompanyname");
+            TextBox txtPhone = (TextBox)row.FindControl("txtPhone");
+            Label lblId = (Label)row.FindControl("lblId");
+
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Password=ITIdev123;Persist Security Info=True;User ID=sa;Initial Catalog=Northwind;Data Source=DESKTOP-GUPHSG0";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Shipper_Update";
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = lblId.Text;
+            cmd.Parameters.Add("@CompanyName", SqlDbType.VarChar).Value = txtCompanyName.Text;
+            cmd.Parameters.Add("@Phone", SqlDbType.VarChar).Value = txtPhone.Text;
+
+            cmd.ExecuteNonQuery();
+            cn.Close();
+
+            gvShipper.EditIndex = -1;
+            GetAll();
+        }
+
+
+        private void GvShipper_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvShipper.EditIndex = -1;
+            GetAll();
+        }
+
+        private void GetAll()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "Password=ITIdev123;Persist Security Info=True;User ID=sa;Initial Catalog=Northwind;Data Source=DESKTOP-GUPHSG0";
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Shipper_GetAll";
+
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            cn.Close();
+
+            this.gvShipper.DataSource = ds.Tables[0];
+            this.gvShipper.DataBind();
+        }
+
+        //private void SetRow()
+        //{
+        //    DataTable dt = new DataTable();
+        //    DataRow dr = null;
+        //    dt.Columns(new DataColumn("RowNumber"));
+        //    dr = dt.NewRow();
+        //    dr["RowNumber"] = 1;
+        //    dt.Rows.Add(dr);
+        //}
+
+        private void GvShipper_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvShipper.EditIndex = e.NewEditIndex;
+            GetAll();
+        }
+    }
+}
